5a48231308c4a2c7c442c5d68a73c304ffbce6e8
Dr. Cleve <76855369+David-Orangemoon@users.noreply.github.com>
2026-01-11 18:11:44 -0500

    Finish attack menu

--- src/deltarune.lua
+++ src/deltarune.lua
@@ -1,3 +1,21 @@
+local boxSize = {
+    x = 544,
+    y = 140,
+
+    targetX = 544,
+    targetY = 140,
+
+    speed = 75
+}
+
+function get_box_bounds()
+    local width = (boxSize.x / 2);
+    local height = (boxSize.y / 2);
+    local y = 319 - math.max(0, height - 140);
+
+    return 320 - width, y - height, 320 + width, y + height;
+end
+
 G.UT_IMAGES = {
     soul = new_arbituary_image("drut/soul.png"),
     battleOpt = new_arbituary_image("drut/battleOpts.png"),
@@ -18,24 +36,54 @@ G.UT_MUSIC = {
 
 G.UT_FONTS = {
     TBTD = new_arbituary_font("drut/fonts/TroubleBeneathTheDome.ttf", 24, "mono"),
-    DETERMINATION = new_arbituary_font("drut/fonts/DTM-Sans.ttf", 24, "mono")
+    DETERMINATION = new_arbituary_font("drut/fonts/DTM-Sans.ttf", 24, "mono"),
+    DETERMINATION_MONO = new_arbituary_font("drut/fonts/DTM-Mono.ttf", 24, "mono")
 }
+
+local projectiles = {};
 
 G.UT_ENEMIES = {
     --TEST
     {
         spr = G.UT_IMAGES.testEnemy,
-        attacks = {},
-        health = 10,
+        attacks = {
+            {
+                wantedWidth = 140,
+                wantedHeight = 140,
+                initilize = function(attack, dt)
+                    
+                end,
 
-        initialText = "* He's here!"
+                update = function(attack, dt)
+                    
+                end
+            }
+        },
+        health = 10,
+        maxHealth = 10,
+        atkShake = 0,
+
+        initialText = "* He's here!",
+        name = "Testy mc Test Face"
     }
 }
+
+function inst_enemy(enemyID)
+    local enemy = G.UT_ENEMIES[enemyID];
+    local created = {};
+
+    for key, value in pairs(enemy) do
+        created[key] = value;
+    end
+
+    return created;
+end
 
 local utCanvas = love.graphics.newCanvas(640, 480);
 local currentSong = nil;
 local activeEnemies = {
-    G.UT_ENEMIES[1]
+    inst_enemy(1),
+    inst_enemy(1)
 };
 
 function playSoundish(soundName)
@@ -120,7 +168,19 @@ soul.OOB_mode = function(dt)
 
     soul.leftHeld = love.keyboard.isDown("left");
     soul.rightHeld = love.keyboard.isDown("right");
+    soul.upHeld = love.keyboard.isDown("up");
+    soul.downHeld = love.keyboard.isDown("down");
     soul.selectHeld = love.keyboard.isDown("z");
+    soul.cancelHeld = love.keyboard.isDown("x");
+end
+
+soul.collide = function()
+    local left, top, right, bottom = get_box_bounds();
+    if (soul.x < left) then soul.x = left;
+    elseif ((soul.x + 20) > right) then soul.x = right - 20; end
+    
+    if (soul.y < top) then soul.y = top;
+    elseif ((soul.y + 20) > bottom) then soul.y = bottom - 20; end
 end
 
 soul.modes = {
@@ -135,37 +195,97 @@ soul.modes = {
         if (love.keyboard.isDown("down")) then soul.y = soul.y + speed; end
         if (love.keyboard.isDown("left")) then soul.x = soul.x - speed; end
         if (love.keyboard.isDown("right")) then soul.x = soul.x + speed; end
+
+        soul.collide();
     end
-}
-
-local boxSize = {
-    x = 544,
-    y = 140,
-
-    targetX = 544,
-    targetY = 140,
-
-    speed = 75
 }
 
 G.UT_MENUS = {
     fight = {
         time = 0,
+        target = 0,
+        subMenu = false,
 
         init = function()
             G.UT_MENUS.fight.time = -1;
-            boxSize.targetX = 560;
+            G.UT_MENUS.fight.target = 0;
+            G.UT_MENUS.fight.subMenu = false;
         end,
         
         update = function(dt)
-            G.UT_MENUS.fight.time = G.UT_MENUS.fight.time + dt;
-            soul.x = -1000;
-            soul.y = -1000;
+            if (G.UT_MENUS.fight.subMenu) then
+                boxSize.targetX = 560;
+
+                G.UT_MENUS.fight.time = G.UT_MENUS.fight.time + dt;
+                soul.x = -1000;
+                soul.y = -1000;
+
+                if (G.UT_MENUS.fight.time >= 1) then
+                    soul.inBox = true;
+                    soul.x = 310;
+                    soul.y = 309;
+                end
+
+                if (love.keyboard.isDown("z") and not soul.selectHeld) then
+                    activeEnemies[G.UT_MENUS.fight.target + 1].health = activeEnemies[G.UT_MENUS.fight.target + 1].health - (10 * (1 - math.abs(G.UT_MENUS.fight.time)));
+                    activeEnemies[G.UT_MENUS.fight.target + 1].atkShake = 3;
+
+                    soul.inBox = true;
+                    soul.x = 310;
+                    soul.y = 309;
+                end
+            else
+                soul.x = 60;
+                soul.y = 272 + (G.UT_MENUS.fight.target * 25);
+
+                if (love.keyboard.isDown("down") and not soul.leftHeld) then 
+                    G.UT_MENUS.fight.target = G.UT_MENUS.fight.target + 1;
+
+                    if (G.UT_MENUS.fight.target > #activeEnemies - 1) then
+                        G.UT_MENUS.fight.target = #activeEnemies - 1;
+                    else
+                        playSoundish("switch");
+                    end
+                end
+
+                if (love.keyboard.isDown("up") and not soul.leftHeld) then 
+                    G.UT_MENUS.fight.target = G.UT_MENUS.fight.target - 1;
+
+                    if (G.UT_MENUS.fight.target < 0) then
+                        G.UT_MENUS.fight.target = 0;
+                    else
+                        playSoundish("switch");
+                    end
+                end
+
+                if (love.keyboard.isDown("x") and not soul.cancelHeld) then
+                    soul.menu = "";
+                    soul.toggle_opt(soul.menuOpt + 1, true);
+                end
+
+                if (love.keyboard.isDown("z") and not soul.selectHeld) then
+                    G.UT_MENUS.fight.subMenu = true;
+                end
+            end
         end,
 
         draw = function()
-            love.graphics.draw(G.UT_IMAGES.battleThing, 45, 255);
-            love.graphics.draw(G.UT_IMAGES.battleBar, (G.UT_MENUS.fight.time * boxSize.x / 2) + 320, 255);
+            if (G.UT_MENUS.fight.subMenu) then
+                love.graphics.draw(G.UT_IMAGES.battleThing, 47, 255);
+                love.graphics.draw(G.UT_IMAGES.battleBar, (G.UT_MENUS.fight.time * boxSize.x / 2) + 320, 255);
+            else
+                for id, ent in pairs(activeEnemies) do
+                    love.graphics.print(" * ".. ent.name, 80, 267 + ((id - 1) * 25), 0);
+
+                    love.graphics.setColor( 1, 0, 0, 1 );
+                    love.graphics.rectangle( "fill", 420, 276 + ((id - 1) * 25), 100, 16);
+
+                    love.graphics.setColor( 0, 1, 0, 1 );
+                    love.graphics.rectangle( "fill", 420, 276 + ((id - 1) * 25), 100, 16);
+
+                    love.graphics.setColor( 1, 1, 1, 1 );
+                end
+            end
         end
     }
 };
@@ -173,7 +293,6 @@ G.UT_MENUS = {
 local curProfile = {};
 
 G.ut_update = function(dt)
-
     --Resizing the middle box
     if (boxSize.x < boxSize.targetX) then
         boxSize.x = boxSize.x + boxSize.speed * dt;
@@ -210,8 +329,19 @@ G.ut_update = function(dt)
         return;
     end
 
-    if not (soul.inBox) then soul.OOB_mode(dt);
-    else soul.modes[soul.mode](dt); end
+    if (boxSize.x == boxSize.targetX and boxSize.y == boxSize.targetY) then
+        if not (soul.inBox) then soul.OOB_mode(dt);
+        else soul.modes[soul.mode](dt); end
+    end
+
+    for id, ent in pairs(activeEnemies) do
+        if (ent.atkShake > 0) then
+            ent.atkShake = ent.atkShake - dt;
+            if (ent.atkShake < 0) then
+                ent.atkShake = 0;
+            end
+        end
+    end
 end
 
 G.play_encounter_music = function()
@@ -239,18 +369,47 @@ G.fams_draw = function()
                 return;
             end
 
-            love.graphics.rectangle( "line", 320 - (boxSize.x / 2), 319 - (boxSize.y / 2), boxSize.x, boxSize.y );
+            local left, top, right, bottom = get_box_bounds();
+            love.graphics.rectangle("line", left, top, right - left, bottom - top);
             love.graphics.draw(G.UT_IMAGES.battleOpt, soul.fight, 45, 426);
             love.graphics.draw(G.UT_IMAGES.battleOpt, soul.act, 191, 426);
             love.graphics.draw(G.UT_IMAGES.battleOpt, soul.item, 337, 426);
             love.graphics.draw(G.UT_IMAGES.battleOpt, soul.mercy, 483, 426);
+
+            love.graphics.setFont(G.UT_FONTS.DETERMINATION_MONO);
         
             love.graphics.draw( G.UT_IMAGES.soul, soul.quad, soul.x, soul.y);
-            if (soul.menu ~= "") then
-                G.UT_MENUS[soul.menu].draw();
-            else
-                love.graphics.setFont(G.UT_FONTS.DETERMINATION);
-                love.graphics.print("* Testicles", 60, 267, 0);
+            if (not soul.inBox) then
+                if (soul.menu ~= "") then
+                    G.UT_MENUS[soul.menu].draw();
+                else
+                    love.graphics.print("* Big Boner Up the Lane", 60, 267, 0);
+                end
+            end
+
+            local offset = 320 - (((#activeEnemies - 1) * 0.5) * 200);
+            for id, ent in pairs(activeEnemies) do
+                local imgType = type(ent.spr);
+
+                if (imgType == "function") then
+                    
+                else 
+                    local width, height = ent.spr:getDimensions();
+                    love.graphics.draw(ent.spr, (offset - width / 2) + math.sin(ent.atkShake * 90) * (ent.atkShake * 1.5), 128 - height / 2);
+                    
+                    --Draw the health bar
+                    if (ent.atkShake > 0) then
+                        love.graphics.setColor(1, 0, 0, 1);
+                        love.graphics.rectangle("fill", offset - 32, 220, 64, 12);
+
+                        love.graphics.setColor(0, 1, 0, 1);
+                        love.graphics.rectangle("fill", offset - 32, 220, (ent.health / ent.maxHealth) * 64, 12);
+
+                        love.graphics.setColor(1, 1, 1, 1);
+                    end
+                end
+
+                offset = offset + 200;
             end
 
             love.graphics.setFont(G.UT_FONTS.TBTD);
